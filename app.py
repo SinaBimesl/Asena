cdc427eedbc2a9f5307f505e4c334a9bca8d2895
SinaBimesl <sinabimesl2000@gmail.com>
2025-08-03 18:06:40 +0330

    Moved new version

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ window = uic.loadUi("dns_ui.ui")
 
 def on_show_current_dns():
     current_dns = show_current_dns()
-    # اگر لیست بود، به رشته تبدیل می‌کنیم
+
     if isinstance(current_dns, list):
         current_dns = ", ".join(current_dns)
     window.label_current_dns.setText(current_dns)
 
-# وصل کردن دکمه به تابع
+
 window.btn_show_current_dns.clicked.connect(on_show_current_dns)
 
-# نمایش پنجره
+
 window.show()
 sys.exit(app.exec_())
